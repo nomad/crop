25be9fa6a86fd78518878075774ccbd342d8d5bd
noib3 <riccardo.mazzarini@pm.me>
2022-11-11 16:42:40 +0100

    slightly more efficient `Bytes::next` implementation

--- src/rope/iterators.rs
+++ src/rope/iterators.rs
@@ -12,22 +12,36 @@ impl<'a> Iterator for Chunks<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         self.chunks.next().map(|s| &*s.text)
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let remaining = self.chunks.len();
+        (remaining, Some(remaining))
+    }
 }
+
+impl<'a> ExactSizeIterator for Chunks<'a> {}
 
 /// TODO: docs
 #[derive(Clone)]
 pub struct Bytes<'a> {
     chunks: Chunks<'a>,
-    current: &'a str,
-    yielded: usize,
+    current: &'a [u8],
+    yielded_in_current: usize,
+    total_yielded: usize,
     total_bytes: usize,
 }
 
 impl<'a> From<&'a Rope> for Bytes<'a> {
     fn from(rope: &'a Rope) -> Self {
         let mut chunks = rope.chunks();
-        let current = chunks.next().unwrap_or_default();
-        Self { chunks, current, yielded: 0, total_bytes: rope.byte_len() }
+        let current = chunks.next().unwrap_or_default().as_bytes();
+        Self {
+            chunks,
+            current,
+            yielded_in_current: 0,
+            total_yielded: 0,
+            total_bytes: rope.byte_len(),
+        }
     }
 }
 
@@ -35,16 +49,25 @@ impl<'a> Iterator for Bytes<'a> {
     type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.current.is_empty() || self.yielded == self.current.len() {
-            self.current = self.chunks.next()?;
-            self.yielded = 0;
+        if self.yielded_in_current == self.current.len() {
+            // NOTE: make sure there are never empty chunks or this will fail.
+            self.current = self.chunks.next()?.as_bytes();
+            self.yielded_in_current = 0;
         }
 
-        let byte = self.current.as_bytes()[self.yielded];
-        self.yielded += 1;
+        let byte = self.current[self.yielded_in_current];
+        self.yielded_in_current += 1;
+        self.total_yielded += 1;
         Some(byte)
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let remaining = self.total_bytes - self.total_yielded;
+        (remaining, Some(remaining))
+    }
 }
+
+impl<'a> ExactSizeIterator for Bytes<'a> {}
 
 #[cfg(test)]
 mod tests {
