3c7a1d1004e060b9e60810ca1a0aae7091718391
noib3 <riccardo.mazzarini@pm.me>
2022-11-12 13:28:34 +0100

    remove the `Leaves::reverse` method, implement `DoubleEndedIterator` for `Leaves`

--- src/tree/iterators.rs
+++ src/tree/iterators.rs
@@ -1,20 +1,24 @@
 use super::{Node, Summarize};
 
-/// TODO: docs
+/// An iterator over the leaves of trees or tree slices.
+///
+/// This iterator is created via the `leaves` method on
+/// [`Tree`](super::Tree::leaves) and [`TreeSlice`](super::TreeSlice::leaves).
 #[derive(Clone)]
 pub struct Leaves<'a, Leaf: Summarize> {
     leaves: Vec<&'a Leaf>,
-    current: usize,
-    iterating_forward: bool,
+    start: usize,
+    end: usize,
 }
 
 impl<'a, Leaf: Summarize> Leaves<'a, Leaf> {
-    pub fn new() -> Self {
-        Self { leaves: Vec::new(), current: 0, iterating_forward: true }
+    pub(super) fn new() -> Self {
+        Self { leaves: Vec::new(), start: 0, end: 0 }
     }
 
     pub(super) fn push_leaf(&mut self, leaf: &'a Leaf) {
-        self.leaves.push(leaf)
+        self.leaves.push(leaf);
+        self.end += 1;
     }
 
     pub(super) fn push_node_subtree<const N: usize>(
@@ -31,44 +35,35 @@ impl<'a, Leaf: Summarize> Leaves<'a, Leaf> {
             Node::Leaf(leaf) => self.push_leaf(leaf.value()),
         }
     }
-
-    /// Reverses the direction of the iterator.
-    pub fn reverse(mut self) -> Self {
-        self.iterating_forward = !self.iterating_forward;
-        self
-    }
 }
 
 impl<'a, Leaf: Summarize> Iterator for Leaves<'a, Leaf> {
     type Item = &'a Leaf;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.iterating_forward {
-            if self.current == self.leaves.len() {
-                None
-            } else {
-                let leaf = &self.leaves[self.current];
-                self.current += 1;
-                Some(leaf)
-            }
+        if self.start == self.end {
+            None
         } else {
-            if self.current == 0 {
-                None
-            } else {
-                let leaf = &self.leaves[self.current];
-                self.current -= 1;
-                Some(leaf)
-            }
+            let leaf = &self.leaves[self.start];
+            self.start += 1;
+            Some(leaf)
         }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = if self.iterating_forward {
-            self.leaves.len() - self.current
+        let remaining = self.end - self.start;
+        (remaining, Some(remaining))
+    }
+}
+
+impl<'a, Leaf: Summarize> DoubleEndedIterator for Leaves<'a, Leaf> {
+    fn next_back(&mut self) -> Option<&'a Leaf> {
+        if self.start == self.end {
+            None
         } else {
-            self.current
-        };
-        (len, Some(len))
+            self.end -= 1;
+            Some(&self.leaves[self.end])
+        }
     }
 }
 
