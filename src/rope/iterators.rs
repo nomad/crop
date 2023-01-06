cff9de0422cbfc596d245e703de33f5f896bec3b
noib3 <me@noib3.dev>
2023-01-06 20:17:28 +0100

    refactor `Bytes`, `Chars`

--- src/rope/iterators.rs
+++ src/rope/iterators.rs
@@ -61,25 +61,22 @@ pub struct Bytes<'a> {
     chunks: Chunks<'a>,
 
     /// TODO: docs
-    current_forward: &'a [u8],
+    chunk_front: &'a [u8],
 
     /// TODO: docs
-    forward_byte_idx: usize,
+    byte_idx_front: usize,
 
     /// TODO: docs
-    current_backward: &'a [u8],
+    chunk_back: &'a [u8],
 
     /// TODO: docs
-    backward_byte_idx: usize,
+    byte_idx_back: usize,
 
     /// TODO: docs
-    yielded_forward: usize,
+    yielded: usize,
 
     /// TODO: docs
-    yielded_backward: usize,
-
-    /// TODO: docs
-    total_bytes: usize,
+    total: usize,
 }
 
 impl<'a> From<&'a Rope> for Bytes<'a> {
@@ -87,13 +84,12 @@ impl<'a> From<&'a Rope> for Bytes<'a> {
     fn from(rope: &'a Rope) -> Self {
         Self {
             chunks: rope.chunks(),
-            current_forward: &[],
-            forward_byte_idx: 0,
-            yielded_forward: 0,
-            current_backward: &[],
-            backward_byte_idx: 0,
-            yielded_backward: 0,
-            total_bytes: rope.byte_len(),
+            chunk_front: &[],
+            byte_idx_front: 0,
+            chunk_back: &[],
+            byte_idx_back: 0,
+            yielded: 0,
+            total: rope.byte_len(),
         }
     }
 }
@@ -103,13 +99,12 @@ impl<'a, 'b: 'a> From<&'a RopeSlice<'b>> for Bytes<'a> {
     fn from(slice: &'a RopeSlice<'b>) -> Self {
         Self {
             chunks: slice.chunks(),
-            current_forward: &[],
-            forward_byte_idx: 0,
-            yielded_forward: 0,
-            current_backward: &[],
-            backward_byte_idx: 0,
-            yielded_backward: 0,
-            total_bytes: slice.byte_len(),
+            chunk_front: &[],
+            byte_idx_front: 0,
+            chunk_back: &[],
+            byte_idx_back: 0,
+            yielded: 0,
+            total: slice.byte_len(),
         }
     }
 }
@@ -119,53 +114,38 @@ impl<'a> Iterator for Bytes<'a> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        if self.yielded_forward + self.yielded_backward == self.total_bytes {
-            return None;
+        if self.byte_idx_front == self.chunk_front.len() {
+            // We've exhausted the current chunk.
+            if let Some(chunk) = self.chunks.next() {
+                self.chunk_front = chunk.as_bytes();
+                self.byte_idx_front = 0;
+            } else {
+                // There are no more chunks but there may still be some
+                // un-yielded bytes on the backward chunk.
+                if self.byte_idx_back == 0 {
+                    return None;
+                } else {
+                    // We return the first byte of the backward chunk, remove
+                    // that byte from the chunk and update the backward byte
+                    // index.
+                    let byte = self.chunk_back[0];
+                    self.chunk_back = &self.chunk_back[1..];
+                    self.byte_idx_back -= 1;
+                    self.yielded += 1;
+                    return Some(byte);
+                }
+            }
         }
 
-        if self.forward_byte_idx == self.current_forward.len() {
-            self.current_forward = match self.chunks.next() {
-                Some(chunk) => {
-                    // NOTE: make sure  there are never empty chunks or this
-                    // will make the byte indexing below fail.
-                    chunk.as_bytes()
-                },
-
-                None => {
-                    // There are no more chunks to get but there may be some
-                    // bytes yet to yielded on the backward chunk.
-                    if self.backward_byte_idx == 0 {
-                        return None;
-                    } else {
-                        // Return the first byte of the backward chunk, make
-                        // it one byte smaller and return.
-
-                        let byte = self.current_backward[0];
-                        self.current_backward = &self.current_backward[1..];
-
-                        // After making the backward chunk 1 byte smaller we
-                        // have to decrease the backward byte idx by one to
-                        // keep those in sync.
-                        self.backward_byte_idx -= 1;
-
-                        self.yielded_forward += 1;
-                        return Some(byte);
-                    }
-                },
-            };
-            self.forward_byte_idx = 0;
-        }
-
-        let byte = self.current_forward[self.forward_byte_idx];
-        self.forward_byte_idx += 1;
-        self.yielded_forward += 1;
+        let byte = self.chunk_front[self.byte_idx_front];
+        self.byte_idx_front += 1;
+        self.yielded += 1;
         Some(byte)
     }
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let exact =
-            self.total_bytes - self.yielded_forward - self.yielded_backward;
+        let exact = self.total - self.yielded;
         (exact, Some(exact))
     }
 }
@@ -173,44 +153,31 @@ impl<'a> Iterator for Bytes<'a> {
 impl<'a> DoubleEndedIterator for Bytes<'a> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        if self.yielded_forward + self.yielded_backward == self.total_bytes {
-            return None;
+        if self.byte_idx_back == 0 {
+            // We've exhausted the current chunk.
+            if let Some(chunk) = self.chunks.next_back() {
+                self.chunk_back = chunk.as_bytes();
+                self.byte_idx_back = chunk.len();
+            } else {
+                // There are no more chunks but there may still be some
+                // un-yielded bytes on the forward chunk.
+                if self.byte_idx_front == self.chunk_front.len() {
+                    return None;
+                } else {
+                    // We return the last byte of the forward chunk and remove
+                    // that byte from the chunk.
+                    let byte_idx = self.chunk_front.len() - 1;
+                    let byte = self.chunk_front[byte_idx];
+                    self.chunk_front = &self.chunk_front[..byte_idx];
+                    self.yielded += 1;
+                    return Some(byte);
+                }
+            }
         }
 
-        if self.backward_byte_idx == 0 {
-            self.current_backward = match self.chunks.next_back() {
-                Some(chunk) => {
-                    // NOTE: make sure  there are never empty chunks or this
-                    // will make the byte indexing below fail.
-                    chunk.as_bytes()
-                },
-
-                None => {
-                    // There are no more chunks to get but there may be some
-                    // bytes yet to yielded on the forward chunk.
-                    if self.forward_byte_idx == self.current_forward.len() {
-                        return None;
-                    } else {
-                        // Return the last byte of the forward chunk, make
-                        // it one byte smaller and return.
-                        let byte = self.current_forward
-                            [self.current_forward.len() - 1];
-
-                        self.current_forward = &self.current_forward
-                            [..self.current_forward.len() - 1];
-
-                        self.yielded_backward += 1;
-
-                        return Some(byte);
-                    }
-                },
-            };
-            self.backward_byte_idx = self.current_backward.len();
-        }
-
-        let byte = self.current_backward[self.backward_byte_idx - 1];
-        self.backward_byte_idx -= 1;
-        self.yielded_backward += 1;
+        self.byte_idx_back -= 1;
+        let byte = self.chunk_back[self.byte_idx_back];
+        self.yielded += 1;
         Some(byte)
     }
 }
@@ -226,26 +193,16 @@ pub struct Chars<'a> {
     chunks: Chunks<'a>,
 
     /// TODO: docs
-    current_forward: &'a str,
-
-    /// Note: this is the number of *bytes* already yielded in `current`, not
-    /// chars.
-    forward_byte_idx: usize,
+    chunk_front: &'a str,
 
     /// TODO: docs
-    current_backward: &'a str,
+    byte_idx_front: usize,
 
     /// TODO: docs
-    backward_byte_idx: usize,
+    chunk_back: &'a str,
 
     /// TODO: docs
-    yielded_forward: usize,
-
-    /// TODO: docs
-    yielded_backward: usize,
-
-    /// TODO: docs
-    total_bytes: usize,
+    byte_idx_back: usize,
 }
 
 impl<'a> From<&'a Rope> for Chars<'a> {
@@ -253,13 +210,10 @@ impl<'a> From<&'a Rope> for Chars<'a> {
     fn from(rope: &'a Rope) -> Self {
         Self {
             chunks: rope.chunks(),
-            current_forward: "",
-            forward_byte_idx: 0,
-            current_backward: "",
-            backward_byte_idx: 0,
-            yielded_forward: 0,
-            yielded_backward: 0,
-            total_bytes: rope.byte_len(),
+            chunk_front: "",
+            byte_idx_front: 0,
+            chunk_back: "",
+            byte_idx_back: 0,
         }
     }
 }
@@ -269,13 +223,10 @@ impl<'a, 'b: 'a> From<&'a RopeSlice<'b>> for Chars<'a> {
     fn from(slice: &'a RopeSlice<'b>) -> Self {
         Self {
             chunks: slice.chunks(),
-            current_forward: "",
-            forward_byte_idx: 0,
-            current_backward: "",
-            backward_byte_idx: 0,
-            yielded_forward: 0,
-            yielded_backward: 0,
-            total_bytes: slice.byte_len(),
+            chunk_front: "",
+            byte_idx_front: 0,
+            chunk_back: "",
+            byte_idx_back: 0,
         }
     }
 }
@@ -285,144 +236,85 @@ impl<'a> Iterator for Chars<'a> {
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
-        if self.yielded_forward + self.yielded_backward == self.total_bytes {
-            return None;
+        if self.byte_idx_front == self.chunk_front.len() {
+            if let Some(chunk) = self.chunks.next() {
+                self.chunk_front = chunk;
+                self.byte_idx_front = 0;
+            } else {
+                // Note: see `Bytes::next` for some relevant comments.
+                if self.byte_idx_back == 0 {
+                    return None;
+                } else {
+                    let ch = self.chunk_back.chars().next();
+
+                    debug_assert!(ch.is_some());
+
+                    // Safety: `byte_idx_back` is greater than zero so there
+                    // are still chars to yield in that chunk.
+                    let ch = unsafe { ch.unwrap_unchecked() };
+
+                    let len = ch.len_utf8();
+                    self.chunk_back = &self.chunk_back[len..];
+                    self.byte_idx_back -= len;
+                    return Some(ch);
+                }
+            }
         }
 
-        if self.forward_byte_idx == self.current_forward.len() {
-            self.current_forward = match self.chunks.next() {
-                Some(chunk) => {
-                    // NOTE: make sure there are never empty chunks or this
-                    // will make the byte indexing below fail.
-                    chunk
-                },
+        let ch = self.chunk_front[self.byte_idx_front..].chars().next();
 
-                None => {
-                    // NOTE: see `Bytes::next` for relevant comments.
+        debug_assert!(ch.is_some());
 
-                    if self.backward_byte_idx == 0 {
-                        return None;
-                    } else {
-                        debug_assert!(self
-                            .current_backward
-                            .chars()
-                            .next()
-                            .is_some());
+        // Safety: `byte_idx_front` is less than the byte length of
+        // `chunk_front`, so there are still chars to yield in this chunk.
+        let ch = unsafe { ch.unwrap_unchecked() };
 
-                        let char = unsafe {
-                            self.current_backward
-                                .chars()
-                                .next()
-                                // Safety: `backward_byte_idx > 0`, so
-                                // there are still chars to yield in this
-                                // chunk.
-                                .unwrap_unchecked()
-                        };
+        self.byte_idx_front += ch.len_utf8();
 
-                        let len = char.len_utf8();
-
-                        self.current_backward = &self.current_backward[len..];
-                        self.backward_byte_idx -= len;
-                        self.yielded_forward += len;
-                        return Some(char);
-                    }
-                },
-            };
-            self.forward_byte_idx = 0;
-        }
-
-        debug_assert!(self.current_forward[self.forward_byte_idx..]
-            .chars()
-            .next()
-            .is_some());
-
-        let char = unsafe {
-            self.current_forward[self.forward_byte_idx..]
-                .chars()
-                .next()
-                // Safety: `forward_byte_idx < current_forward.len()`, so there
-                // are still chars to yield in this chunk.
-                .unwrap_unchecked()
-        };
-
-        let len = char.len_utf8();
-        self.forward_byte_idx += len;
-        self.yielded_forward += len;
-
-        Some(char)
+        Some(ch)
     }
 }
 
 impl<'a> DoubleEndedIterator for Chars<'a> {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
-        if self.yielded_forward + self.yielded_backward == self.total_bytes {
-            return None;
+        if self.byte_idx_back == 0 {
+            if let Some(chunk) = self.chunks.next_back() {
+                self.chunk_back = chunk;
+                self.byte_idx_back = self.chunk_back.len();
+            } else {
+                // Note: see `Bytes::next_back` for some relevant comments.
+                if self.byte_idx_front == self.chunk_front.len() {
+                    return None;
+                } else {
+                    let ch = self.chunk_front.chars().next_back();
+
+                    debug_assert!(ch.is_some());
+
+                    // Safety: `byte_idx_front` is less than the byte length of
+                    // `chunk_front`, so there are still chars to yield in that
+                    // chunk.
+                    let ch = unsafe { ch.unwrap_unchecked() };
+
+                    self.chunk_front = &self.chunk_front
+                        [..self.chunk_front.len() - ch.len_utf8()];
+
+                    return Some(ch);
+                }
+            }
         }
 
-        if self.backward_byte_idx == 0 {
-            self.current_backward = match self.chunks.next_back() {
-                Some(chunk) => {
-                    // NOTE: make sure there are never empty chunks or this
-                    // will make the byte indexing below fail.
-                    chunk
-                },
+        let ch = self.chunk_back[..self.byte_idx_back].chars().next_back();
 
-                None => {
-                    // NOTE: see `Bytes::next_back` for relevant comments.
+        debug_assert!(ch.is_some());
 
-                    if self.forward_byte_idx == self.current_forward.len() {
-                        return None;
-                    } else {
-                        debug_assert!(self
-                            .current_forward
-                            .chars()
-                            .next_back()
-                            .is_some());
+        // Safety: `byte_idx_back` is greater than zero so there
+        // are still chars to yield in this chunk.
+        let ch = unsafe { ch.unwrap_unchecked() };
 
-                        let char = unsafe {
-                            self.current_forward
-                                .chars()
-                                .next_back()
-                                // Safety: `forward_byte_idx <
-                                // current_forward.len()`, so there are still
-                                // chars to yield in this chunk.
-                                .unwrap_unchecked()
-                        };
+        self.byte_idx_back -= ch.len_utf8();
 
-                        let len = char.len_utf8();
-
-                        self.current_forward = &self.current_forward
-                            [..self.current_forward.len() - len];
-
-                        self.yielded_backward += len;
-
-                        return Some(char);
-                    }
-                },
-            };
-            self.backward_byte_idx = self.current_backward.len();
-        }
-
-        debug_assert!(self.current_backward[..self.backward_byte_idx]
-            .chars()
-            .next_back()
-            .is_some());
-
-        let char = unsafe {
-            self.current_backward[..self.backward_byte_idx]
-                .chars()
-                .next_back()
-                // Safety: `backward_byte_idx > 0`, so there are still chars to
-                // yield in this chunk.
-                .unwrap_unchecked()
-        };
-
-        let len = char.len_utf8();
-        self.backward_byte_idx -= len;
-        self.yielded_backward += len;
-
-        Some(char)
+        Some(ch)
     }
 }
 
